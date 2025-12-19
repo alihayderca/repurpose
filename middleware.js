8dfce088ecab0c1c91e3789a457edc5013a11fc2
Ali Hayder <alihayderca@gmail.com>
2025-12-19 11:40:53 -0500

    Make SEO pages public

--- middleware.js
+++ middleware.js
@@ -2,7 +2,15 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Public routes that don't require auth
-  publicRoutes: ["/"],
+  publicRoutes: [
+    "/",
+    "/privacy",
+    "/terms",
+    "/twitter-thread-generator",
+    "/linkedin-post-generator",
+    "/threads-post-generator",
+    "/success",
+  ],
 });
 
 export const config = {
